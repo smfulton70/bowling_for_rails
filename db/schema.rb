9ef493a969b168ffb1169b0b3027a45c7c74aa93
Scott Fulton <scottfulton@MacBook-Pro.localdomain>
2019-07-29 13:45:16 -0700

    establish previous frames in the frame model and ensure that frame new show page shows current frame

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_001845) do
   create_table "frames", force: :cascade do |t|
     t.integer "ball_one", default: 0
     t.integer "ball_two", default: 0
-    t.integer "ball_three", default: 0
+    t.integer "ball_three"
     t.integer "frame_number", default: 0
     t.integer "frame_score", default: 0
     t.boolean "spare", default: false
